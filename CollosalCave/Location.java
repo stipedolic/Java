c7068fbedaeb76c047eb0c927207f381902fbe17
stipedolic <80953273+stipedolic@users.noreply.github.com>
2021-04-27 19:56:51 +0200

    Add files via upload

--- CollosalCave/Location.java
+++ CollosalCave/Location.java
@@ -1,33 +1,28 @@
 package academy.leanprogramming;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public final class Location {
-    private final int locationID;
+public class Location {
     private final String description;
-    private final Map<String, String> exits;
+    private final Map<String, Integer> exits;
 
-    public Location(int locationID, String description) {
-        this.locationID = locationID;
+    public Location( String description) {
         this.description = description;
-        this.exits = new HashMap<>();
+        this.exits = new HashMap<String, Integer>();
+        exits.put("Q", 0);
     }
 
-    public void addExit(String direction, String location){
+    public void addExit(String direction, int location){
         exits.put(direction, location);
     }
 
-    public Map<String, String> getExits() {
-        return exits;
-    }
-
-    public int getLocationID() {
-        return locationID;
-    }
 
     public String getDescription() {
         return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<>(exits);
     }
 }
--- CollosalCave/Main.java
+++ CollosalCave/Main.java
@@ -2,40 +2,75 @@ package academy.leanprogramming;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
-
+    public static Map<Integer, Location> locations = new HashMap<>();
+    public static Map<String, String> vocabulary = new HashMap<>();
     public static void main(String[] args) {
-        Location location = new Location(0, "Bezvezni opis");
-        location.addExit("S", "south");
-        location.addExit("W", "west");
-        location.addExit("N", "north");
+        Scanner scanner = new Scanner(System.in);
+        locations.put(0, new Location( "You are sitting in front of a computer learning Java"));
+        locations.put(1, new Location( "You are standing at the end of a road before a small brick building"));
+        locations.put(2, new Location( "You are at the top of a hill"));
+        locations.put(3, new Location( "You are inside a building, a well house for a small spring"));
+        locations.put(4, new Location( "You are in a valley beside a stream"));
+        locations.put(5, new Location( "You are in the forest"));
+
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("NORTH", "N");
 
 
+        locations.get(1).addExit("W", 2);
+        locations.get(1).addExit("E", 3);
+        locations.get(1).addExit("S", 4);
+        locations.get(1).addExit("N", 5);
+      //  locations.get(1).addExit("Q", 0);
 
-        Map<String, String> exits = location.getExits();
-        Map<String, String> blabla = new HashMap<>();
+        locations.get(2).addExit("N", 5);
+     //   locations.get(2).addExit("Q", 0);
+
+        locations.get(3).addExit("W", 1);
+     //   locations.get(3).addExit("Q", 0);
+
+        locations.get(4).addExit("N", 1);
+        locations.get(4).addExit("W", 2);
+     //   locations.get(4).addExit("Q", 0);
+
+        locations.get(5).addExit("S", 1);
+        locations.get(5).addExit("W", 2);
+     //   locations.get(5).addExit("Q", 0);
 
 
-        for(String exit: exits.keySet()){
-            System.out.print(exit + " ");
-        }
-        System.out.println();
+        int loc = 1;
+        while(true){
+            System.out.println(locations.get(loc).getDescription());
+            if(loc == 0){
+                break;
+            }
+            Map<String, Integer> exits = locations.get(loc).getExits();
+            System.out.println("Available exits are:");
+            for(String s : exits.keySet()){
+                System.out.print(s + ", ");
+            }
+            System.out.println();
 
-        exits.put("E", "east");
-        blabla = location.getExits();
+            String direction = scanner.nextLine().toUpperCase();
+            String[] words = direction.split(" ");
 
-        for(String bla: blabla.keySet()){
-            System.out.print(bla + " ");
-        }
-        System.out.println();
-        for(String exit: exits.keySet()){
-            System.out.print(exit + " ");
-        }
-        System.out.println();
-        for(String exit: location.getExits().keySet()){
-            System.out.print(exit + " ");
-        }
-
+            for(String word: words){
+                if(vocabulary.containsKey(word)){
+                    direction = vocabulary.get(word);
+                    break;
+                }
+            }
+            if(exits.containsKey(direction)){
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction.");
+            }
+           }
     }
 }
